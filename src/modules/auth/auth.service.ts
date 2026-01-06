4f415bfe3a6ea6ea179778d6e38f7b1d485ef631
arifuddincoder <arifuddincoder@gmail.com>
2026-01-06 08:04:17 +0600

    added role in user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -18,11 +18,11 @@ const loginUser = async (email: string, password: string) => {
 		return false;
 	}
 
-	const token = jwt.sign({ name: user.name, email: user.email }, config.jwtSecret as string, {
+	const token = jwt.sign({ name: user.name, email: user.email, role: user.role }, config.jwtSecret as string, {
 		expiresIn: "7d",
 	});
 
-	console.log({ token });
+	// console.log({ token });
 
 	return { token, user };
 };
